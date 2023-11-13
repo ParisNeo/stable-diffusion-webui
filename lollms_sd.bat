4782644d936dbcc72c8aa9f74996efc36f72553c
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2023-11-13 10:40:31 +0100

    Update lollms_sd.bat

--- lollms_sd.bat
+++ lollms_sd.bat
@@ -50,14 +50,14 @@ set ACCELERATE="%VENV_DIR%\Scripts\accelerate.exe"
 if EXIST %ACCELERATE% goto :accelerate_launch
 
 :launch
-%PYTHON% launch.py %* --listen
+%PYTHON% launch.py %*
 if EXIST tmp/restart goto :skip_venv
 pause
 exit /b
 
 :accelerate_launch
 echo Accelerating
-%ACCELERATE% launch --num_cpu_threads_per_process=6 launch.py --listen
+%ACCELERATE% launch --num_cpu_threads_per_process=6 launch.py
 if EXIST tmp/restart goto :skip_venv
 pause
 exit /b
